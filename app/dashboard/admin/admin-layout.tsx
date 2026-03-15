2e8853c7e6ae309e6798946f72e0da5cc085cbe9
hamza-jaffar <hamzajaffer231006@gmail.com>
2026-03-15 10:44:20 +0500

    fix: build breadcurms problem

--- app/dashboard/admin/admin-layout.tsx
+++ app/dashboard/admin/admin-layout.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Package, FolderTree, LayoutDashboard, Settings, X, Menu, ChevronLeft } from "lucide-react";
 import Image from "next/image";
-import Breadcrumb from "@/components/breadcrumb";
+import Breadcrumb from "@/components/Breadcrumb";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
